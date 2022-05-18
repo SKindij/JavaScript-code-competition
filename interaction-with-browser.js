3871a81b54b8126c4d8f082b5456da62001169fb
SKindij <88454706+SKindij@users.noreply.github.com>
2022-05-18 19:28:42 +0300

    Update interaction-with-browser.js

--- interaction-with-browser.js
+++ interaction-with-browser.js
@@ -46,8 +46,51 @@ someElement.addEventListener('click', function (event) {
   console.log('Click!')
 }, { once: true })
 
+// dynamic creation and deletion of paragraphs on the page
+/*
+<html>
+  <head></head>
+  <body >
+    <h1>Living Collection</h1>
+    <h3>При загрузке страницы мы один раз получили все параграфы на странице с помощью getElementsByTagName, их список поддерживается актуальным автоматически</h3>
+    <h2>Сейчас в коллекции: <span id="counter"></span></h2>
+    <div id="article">
+      <p>Это параграф #1</p>
+      <p>Это параграф #2</p>
+    </div>
+    <button id="add">Добавить параграф </button>
+    <button id="remove">Удалить параграф</button>
+  </body>
+</html>
 
 
+
+*/
+
+function createParagraphElement(number) {
+      let newP = document.createElement('p');
+      newP.textContent = 'Это параграф #' + number;
+      return newP;
+    }
+
+    let paragraphs = document.getElementsByTagName('p'); // получаем один раз при загрузке страницы!
+    let articleDiv = document.getElementById('article');
+    let counterSpan = document.getElementById('counter');
+    let addButton = document.getElementById('add');
+    let removeButton = document.getElementById('remove');
+
+    counterSpan.textContent = paragraphs.length;
+
+    addButton.addEventListener('click', function() {
+      articleDiv.appendChild(createParagraphElement(paragraphs.length + 1));
+      counterSpan.textContent = paragraphs.length;
+    });
+
+    removeButton.addEventListener('click', function() {
+      articleDiv.removeChild(paragraphs[paragraphs.length-1]);
+
+      counterSpan.textContent = paragraphs.length;
+    });
 
 
 
