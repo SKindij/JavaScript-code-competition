d8f450a89e4835efe8209997aa5fe892a4c92b8b
SKindij <88454706+SKindij@users.noreply.github.com>
2022-05-27 13:28:55 +0300

    Update promise.js

--- promise.js
+++ promise.js
@@ -1,5 +1,5 @@
 console.log('request data...');
-
+/* below in comments is the somewhat cumbersome approach
 setTimeout(() => {
   console.log('preparing data...');
   
@@ -14,4 +14,18 @@ setTimeout(() => {
     console.log('data received', backendData);
   }, 2000);
 }, 2000);
+*/
+const p = new Promise(function(resolve, reject) {
+  setTimeout(() => {
+    console.log('preparing data...');
+      const backendData = {
+        server: 'aws',
+        port: 2000,
+        status: 'working'
+      }
+  }, 2000);
+});
+
+
+
 
