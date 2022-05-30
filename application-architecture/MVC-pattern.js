ef4dcc39438d7cf3bbfcccc4b6d272834b9e659c
SKindij <88454706+SKindij@users.noreply.github.com>
2022-05-30 17:05:31 +0300

    add flashLightController

--- application-architecture/MVC-pattern.js
+++ application-architecture/MVC-pattern.js
@@ -2,10 +2,34 @@
 // It will have two states: on and off. It will also have day and night light buttons.
 // In the model, we will hold the state of the flashlight.
 const flashLightModel = {
-  isOn: false,
-  color: "blue",
+  isOn: false, // When the user turns on the flashlight, this field should be set to true.
+  color: "blue", // This field contains the color of the flashlight.
 };
-// When the user turns on the flashlight, the isOn field should be set to true, which will be the responsibility of the controller.
+
+// The controller accepts commands from the user and transforms the data into models according to those commands.
+const flashLightController1 = {
+  toggle() {
+    flashLightModel.isOn = !flashLightModel.isOn;
+  },
+};
+
+// we can switch color with special buttons
+const flashLightController2 = {
+  selectColor(e) {
+    const buttonName = e.target.name;
+      const buttonColors = {
+        daylight: "blue",
+        nightlight: "yellow",
+      };
+    const preferredColor = buttonColors[buttonName];
+    flashLightModel.color = preferredColor;
+  },
+};
+
+
+
+
+
 
 
 
