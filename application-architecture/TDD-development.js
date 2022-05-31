ed1ff8d7f6c204f96f399348a057c3e75ed7336b
SKindij <88454706+SKindij@users.noreply.github.com>
2022-05-31 10:21:29 +0300

    first test for the base case

--- application-architecture/TDD-development.js
+++ application-architecture/TDD-development.js
@@ -6,6 +6,14 @@ import {divide} from 'calculator';
   divide(10, 3, {precision: 2}); // 3.33
   divide(10, 0); // ERROR: I'm sorry. I'm afraid I can't do that.
 
+// According to TDD, we must first write the test, and only then the implementation.
+// ================================================================================
+//the first test for the base case when the numbers are divided by each other without a remainder and the divisor is not 0.
+describe('when given 2 numbers', () => {
+  it.todo('returns the result of dividing the first by the second')
+})
+
+
 
 
 
