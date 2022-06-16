4efad1a58ee1d13b619125fe1ecf15cb60e93045
SKindij <88454706+SKindij@users.noreply.github.com>
2022-06-16 08:39:49 +0300

    Update spaceXdata.js

--- AJAX/spaceXdata.js
+++ AJAX/spaceXdata.js
@@ -1,9 +1,20 @@
 // Consecutive requests could be called inside the call back.
-let dragonXHR = new XMLHttpRequest();
-  dragonXHR.onload = function() {
-    const dragons = JSON.parse(this.responseText);
-    const id = dragons[0].id;
-      console.log(id);
-    
-
+let dragonsXHR = new XMLHttpRequest();
+dragonXHR.onload = function() {
+  const dragons = JSON.parse(this.responseText);
+  const id = dragons[0].id;
+    console.log(id);  
+  let xhr = new XMLHttpRequest();
+  xhr.onload = function() {
+    console.log(JSON.parse(this.responseText));
+  };
+  xhr.open("GET", 'https://api.spacexdata.com/v4/dragons/' + id);
+  xhr.send();
+};
+  
+dragonsXHR.open("GET", 'https://api.spacexdata.com/v4/dragons/');
+dragonsXHR.send();  
+  
+  
+  
 
