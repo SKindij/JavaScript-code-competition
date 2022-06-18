845ffa88c559a4568945e3f6cf5e6d74a75e6095
SKindij <88454706+SKindij@users.noreply.github.com>
2022-06-18 17:06:25 +0300

    Create content-loading-scroll.js

---
+++ Standard-Solutions/content-loading-scroll.js
@@ -0,0 +1,15 @@
+// On the web, it is considered good practice not to load anything extra into the browser.
+// There are many strategies for dealing with redundant code: code splitting and minification, image optimization, caching, refactoring and removal of old code.
+
+// Let's create the database first:
+const post = {
+  title: "Post title",
+  body:
+    "The text of the post in the world's best social network Switter.",
+  likes: 77,
+  reposts: 7,
+}
+
+
+
+
