f4bcdcf8ab6c7f80413a7aca5ec2228f417bd463
SKindij <88454706+SKindij@users.noreply.github.com>
2022-06-18 17:12:02 +0300

    create an "API server"

--- Standard-Solutions/content-loading-scroll.js
+++ Standard-Solutions/content-loading-scroll.js
@@ -10,6 +10,29 @@ const post = {
   reposts: 7,
 }
 
+// Now let's create an "API server":
+const server = {
+  // The page argument is the number of the page to load.
+  posts(page = 1) {
+    const finished = page >= 5
+    // It indicates which page will be next in a row.
+    const next = finished ? null : page + 1
+      // We send an array of 5 post objects as posts.
+      const posts = Array(5).fill(post)
+
+    return new Promise((resolve) => {
+      // Timeout simulates network "latency".
+      setTimeout(() => {
+        resolve({ posts, next })
+      }, 150)
+    })
+  },
+}
+
+
+
+
+
 
 
 
