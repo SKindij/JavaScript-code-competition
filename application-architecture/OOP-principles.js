b4de008ced3d23f798ce9ccf23d82d4fd3ca7343
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2022-06-25 13:44:53 +0300

    Update OOP-principles.js

--- application-architecture/OOP-principles.js
+++ application-architecture/OOP-principles.js
@@ -3,8 +3,28 @@ class Character {
   constructor (power) {
     this.power = power;
   }
-  sayPower = () => console.log(`My power is ${this.power}!`)
+  sayPower = () => console.log(`My power is ${this.power}!`);
 }  
+
+class HeroCharacter extends Character {
+  constructor (power, phrase) {
+    super(power);
+    this.phrase = phrase;
+  }
+  sayPhrase = () => console.log(this.phrase);
+}  
+
+class TopHero extends HeroCharacter {
+  #secretPower;
+  constructor (power, phrase, secretPower) {
+    super(power);
+    this.phrase = phrase;
+    this.#secretPower = secretPower;
+  }
+  sayPhrase = () => console.log(`My top hero speech is: ${this.phrase}!!!`);
+  saySecretPower = () => console.log(this.#secretPower);
+}
+
 
 
 
