0d91b088063464a0e0c79cecf865db2461776b63
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2022-06-25 13:36:45 +0300

    Create OOP-principles.js

---
+++ application-architecture/OOP-principles.js
@@ -0,0 +1,10 @@
+// Inheritance - the ability to acquire the properties of existing classes and create child ones.
+class Character {
+  constructor (power) {
+    this.power = power;
+  }
+  sayPower = () => console.log(`My power is ${this.power}!`)
+}  
+
+
+
