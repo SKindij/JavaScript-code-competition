e44e076bd3c1f3d1c046bc37045eb88ba87b9ffc
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2022-06-27 20:52:28 +0300

    Update and rename OOP-principles.js to OOP-principles-patterns.js

--- application-architecture/OOP-principles-patterns.js
+++ application-architecture/OOP-principles-patterns.js
@@ -245,6 +245,29 @@ const carWithOwner = Object.create(carUA, {
 console.log(carWithOwner.__proto__ === carUA)
 carWithOwner.init()
 
+// SINGLETON
+class Database {
+  constructor(data) {
+    if (Database.exists) {
+      return Database.instance
+    }
+    Database.instance = this
+    Database.exists = true
+    this.data = data
+  }
+
+  getData() {
+    return this.data
+  }
+}
+
+const mongo = new Database('MongoDB')
+console.log(mongo.getData())
+
+const mysql = new Database('MySQL')
+console.log(mysql.getData())
+
+
 
 
 
