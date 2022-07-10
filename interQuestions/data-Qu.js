1a4fb82e3947f7a3e80b8d8d2d20bdc22ab1a57a
SKindij <serhii.kindij@gmail.com>
2022-07-10 21:15:59 +0300

    starting with data tasks

---
+++ interQuestions/data-Qu.js
@@ -0,0 +1,23 @@
+// How can you get the first word the string?
+const text = 'Hello world';
+
+text.slice(0,5);
+text.replace(' world', '');
+text.split(' ')[0];
+
+// what statements are true?
+const a = NaN;
+a == a; // => false
+
+const b = {}; 
+b == b; // => true
+
+typeof null == 'object'; // => true
+!![] == true; 	// => true
+
+
+
+
+
+
+
