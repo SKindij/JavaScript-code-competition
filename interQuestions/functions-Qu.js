cf3fee32b0a5c397cb47bc5960eec3630953c24a
SKindij <serhii.kindij@gmail.com>
2022-07-10 22:20:25 +0300

    added async file

--- interQuestions/functions-Qu.js
+++ interQuestions/functions-Qu.js
@@ -31,6 +31,25 @@ function func(x) {
 func();  // => undefined, 0
 func('1', '2', '3'); // => string, 3
 
+// What will be in the console?
+var num = 2;
+function func(num) {
+  var num;
+    console.log(num);
+    num = 5;
+}
+func(10);  // => 10
+console.log(num); // => 2
+
+// What will be returned by code?
+var answer = "red";
+function f() {this.answer = "green"; return "brown";};
+function f(val) {return "blue";}
+alert (new f().answer);  // => undefined
+
+
+
+
 
 
 
